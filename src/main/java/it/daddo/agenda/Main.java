b3cbb3b7e0ccd021e2c7e4542077480926d597f7
Davide <david.cascella.5@gmail.com>
2022-04-07 19:17:11 +0200

    init login gui

--- src/main/java/it/daddo/agenda/Main.java
+++ src/main/java/it/daddo/agenda/Main.java
@@ -4,6 +4,10 @@
  */
 package it.daddo.agenda;
 
+import com.formdev.flatlaf.FlatDarkLaf;
+import it.daddo.agenda.gui.login.LoginFrame;
+import javax.swing.UIManager;
+
 /**
  *
  * @author Anna
@@ -11,5 +15,17 @@ package it.daddo.agenda;
 public class Main {
     public static void main(String[] args) {
         System.out.println("");
+        FlatDarkLaf.installLafInfo();
+
+        try {
+            UIManager.setLookAndFeel(new FlatDarkLaf());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+         java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new LoginFrame().setVisible(true);
+            }
+        });
     }
 }
